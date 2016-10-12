3e54b0fb5cebca7cf4d15b9273a7e4692194616c
Francis Tseng <f+accounts@frnsys.com>
2016-10-12 15:36:04 -0400

    hiding challenge HUD item for now, gets in the way of the UI

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ import Manager from 'app/Manager';
 Manager.game.state.add('Boot', new Boot());
 Manager.game.state.start('Boot');
 
-var VERSION = 'ALPHA VERSION (0.2.0)';
+var VERSION = 'ALPHA VERSION (0.2.1)';
 document.getElementById('version-notice').innerHTML = VERSION;

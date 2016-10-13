8cbdc91679ee374ddee887a8626c084190dcbf2c
Francis Tseng <f+accounts@frnsys.com>
2016-10-13 13:31:33 -0400

    version bump

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ import Manager from 'app/Manager';
 Manager.game.state.add('Boot', new Boot());
 Manager.game.state.start('Boot');
 
-var VERSION = 'ALPHA VERSION (0.2.2)';
+var VERSION = 'ALPHA VERSION (0.2.3)';
 document.getElementById('version-notice').innerHTML = VERSION;
